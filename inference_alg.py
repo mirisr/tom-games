1d0a214fc70ca783a7d1ed0b85fa0e4a11df2aa3
Iris Seaman <irisrseaman@gmail.com>
2017-11-18 15:29:06 -0700

    added inference algorithms script - importance sampling and MH

---
+++ inference_alg.py
@@ -0,0 +1,47 @@
+import program_trace as program_trace
+from random import randint
+import random
+
+# importance sampling using the score of trace
+def importance_sampling(Q, particles):
+	traces = []
+	scores = np.arange(particles)
+
+	for i in xrange(particles):
+		#deep copy is not working -- this should be fixed -- double check iris
+		score, trace_vals = Q.run_model()
+		traces.append(copy.deepcopy(trace_vals))
+		scores[i] = score
+
+	# get weight for score
+	weights = np.exp(scores - logsumexp(scores))
+
+	# sample
+	chosen_index = np.random.choice([i for i in range(particles)], p=weights)
+	return traces[chosen_index]
+
+
+def metroplis_hastings(Q, particles):
+	traces = []
+	scores = np.arange(particles)
+
+	current_score, current_trace = Q.run_model()
+	for i in xrange(particles):
+
+		# sample from proposal distribution
+		proposed_score, proposed_trace = Q.run_model()
+		# keep
+		traces.append(copy.deepcopy(trace_vals))
+		scores[i] = score
+
+		# accept new sample
+		if log(random.uniform(0,1)) < proposed_score - current_score:
+			current_trace = proposed_trace
+			current_score = current_score
+
+	return current_trace
+
+
+
+
+
