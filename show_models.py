2ca148abb70cb9f4ba965dfb891eb382906f8bd5
Iris Seaman <irisrseaman@gmail.com>
2017-11-18 15:29:57 -0700

    added simulation tests script

---
+++ show_models.py
@@ -0,0 +1,157 @@
+from enforcer import *
+from methods import load_isovist_map, scale_up, direction, dist, detect, load_segs, get_clear_goal,point_in_obstacle
+from my_rrt import *
+import isovist as i
+from random import randint
+import matplotlib
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import cPickle
+from team_runner import TeamRunner
+
+
+def plot(poly_map, plot_name=None, locs=None):
+	fig = plt.figure(1)
+	fig.clf()
+	ax = fig.add_subplot(1, 1, 1)
+	scale = 1
+
+	# plot map
+	x1,y1,x2,y2 = poly_map
+	#x1,y1,x2,y2 = polygons_to_segments( load_polygons( "./map_2.txt" ) )
+	for i in xrange(x1.shape[0]):
+		ax.plot( [ x1[i,0] * scale, x2[i,0] * scale ], [ y1[i,0] * scale, y2[i,0] * scale], 'black' )
+
+
+	add_locations = True
+	if add_locations:
+		for i in xrange(len(locs)):
+			ax.scatter( locs[i][0],  locs[i][1] , color="Green", s = 70, marker='+', linestyle='-')
+			ax.scatter( locs[i][0],  locs[i][1] , s = 95, facecolors='none', edgecolors='g')
+
+	
+	if plot_name is None:
+		plot_name = str(int(time.time()))+".eps"
+
+	ax.set_ylim(ymax = 1, ymin = 0)
+	ax.set_xlim(xmax = 1, xmin = 0)
+
+	#plt.show()
+	fig.savefig(plot_name, bbox_inches='tight')
+
+def setup_plot(poly_map, locs=None):
+	fig = plt.figure(1)
+	fig.clf()
+	ax = fig.add_subplot(1, 1, 1)
+	scale = 1
+
+	# plot map
+	x1,y1,x2,y2 = poly_map
+	for i in xrange(x1.shape[0]):
+		ax.plot( [ x1[i,0] * scale, x2[i,0] * scale ], [ y1[i,0] * scale, y2[i,0] * scale], 'black' )
+
+	# show possible start and goal locations
+	add_locations = True
+	if add_locations:
+		for i in xrange(len(locs)):
+			ax.scatter( locs[i][0],  locs[i][1] , color="Green", s = 70, marker='+', linestyle='-')
+			ax.scatter( locs[i][0],  locs[i][1] , s = 95, facecolors='none', edgecolors='g')
+	return fig, ax
+
+def close_plot(fig, ax, plot_name=None):
+	if plot_name is None:
+		plot_name = str(int(time.time()))+".eps"
+
+	ax.set_ylim(ymax = 1, ymin = 0)
+	ax.set_xlim(xmax = 1, xmin = 0)
+
+	#plt.show()
+	fig.savefig(plot_name, bbox_inches='tight')
+
+def plot_runner(poly_map, trace, locs=None):
+	fig, ax = setup_plot(poly_map, locs)
+
+	# get time
+	t = trace["t"]
+
+	# plot runner's plan
+	path = trace["runner_plan"]
+	for i in range( 0, len(path)-1):
+		ax.plot( [path[i][0], path[i+1][0] ], [ path[i][1], path[i+1][1]], 
+			'black', linestyle=":", linewidth=2)
+	# mark the runner at time t on its plan
+	ax.scatter( path[t][0],  path[t][1] , s = 95, facecolors='none', edgecolors='b')
+
+	# plot partner's plan
+	path = trace["partner_plan"]
+	for i in range( 0, len(path)-1):
+		ax.plot( [path[i][0], path[i+1][0] ], [ path[i][1], path[i+1][1]], 
+			'grey', linestyle="--", linewidth=2)
+	# mark the parter on its plan on time t
+	ax.scatter( path[t][0],  path[t][1] , s = 95, facecolors='none', edgecolors='orange')
+
+
+
+	close_plot(fig, ax)
+
+
+def simulate_running_goal_inference(Q, poly_map, locs):
+	x1,y1,x2,y2 = poly_map
+	# plan for the runner
+	start = 4
+	goal = 0
+	path = planner.run_rrt_opt( np.atleast_2d(locs[start]), 
+		np.atleast_2d(locs[goal]), x1,y1,x2,y2 )
+
+	fig, ax = setup_plot(poly_map, locs)
+
+	for i in range( 0, len(path)-1):
+		ax.plot( [path[i][0], path[i+1][0] ], [ path[i][1], path[i+1][1]], 
+			'lightgrey', linestyle="-", linewidth=1)
+
+	#for t in xrange(15):
+	#	for 
+
+
+	close_plot(fig, ax)
+
+
+
+
+if __name__ == '__main__':
+	#plot("test.eps")
+	# setup
+	locs_map_2 = [[0.4, 0.062] ,
+			[0.964, 0.064] ,
+			[0.442, 0.37] ,
+			[0.1, 0.95] ,
+			[0.946, 0.90] ,
+			[0.066, 0.538]]
+
+	locs = locs_map_2
+	poly_map  = polygons_to_segments( load_polygons( "./map_2.txt" ) )
+	isovist = i.Isovist( load_isovist_map() )
+
+	runner_model = TeamRunner(seg_map=poly_map, locs=locs, isovist=isovist)
+	Q = p.ProgramTrace(runner_model)
+
+	simulate_running_goal_inference(Q, poly_map, locs)
+
+
+	# Q.condition("run_start", 0)
+	# Q.condition("partner_run_start", 4)
+
+	# score, trace = Q.run_model()
+
+	# print score
+	# #print trace["partner_plan"]
+	# #print trace["runner_plan"]
+	# print "detected:", trace["partner_detected"]
+	# print "time:", trace["t"]
+	# print "start:", trace["run_start"]
+	# print "goal:", trace["run_goal"]
+	# plot_runner(poly_map, trace, locs)
+	
+
+	#plot(poly_map, locs=locs_map_2)
+
