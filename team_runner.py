7ba62986a628de7445a50aac89bcfe5e7d39ac41
Iris Seaman <irisrseaman@gmail.com>
2017-12-02 11:19:29 -0700

    fixed flip erp bug - caused incorred scores

--- team_runner.py
+++ team_runner.py
@@ -88,24 +88,26 @@ class BasicRunnerPOM(object):
 
 
 
-		#------------- model other agent movements (past and future) -------------- XXX need to change RV names
-		o_start_i = Q.choice( p=1.0/self.cnt*np.ones((1,self.cnt)), name="other_run_start" )
-		o_goal_i = Q.choice( p=1.0/self.cnt*np.ones((1,self.cnt)), name="other_run_goal" )
-		o_start = np.atleast_2d( self.locs[o_start_i] )
-		o_goal = np.atleast_2d( self.locs[o_goal_i] )
+		# #------------- model other agent movements (past and future) -------------- XXX need to change RV names
+		# o_start_i = Q.choice( p=1.0/self.cnt*np.ones((1,self.cnt)), name="other_run_start" )
+		# o_goal_i = Q.choice( p=1.0/self.cnt*np.ones((1,self.cnt)), name="other_run_goal" )
+		# o_start = np.atleast_2d( self.locs[o_start_i] )
+		# o_goal = np.atleast_2d( self.locs[o_goal_i] )
 
-		# plan using the latent variables of start and goal
-		other_plan = planner.run_rrt_opt( np.atleast_2d(o_start), 
-		np.atleast_2d(o_goal), rx1,ry1,rx2,ry2 )
+		# # plan using the latent variables of start and goal
+		# other_plan = planner.run_rrt_opt( np.atleast_2d(o_start), 
+		# np.atleast_2d(o_goal), rx1,ry1,rx2,ry2 )
 
-		# add noise to the plan
-		other_noisy_plan = [other_plan[0]]
-		for i in xrange(1, len(other_plan)-1):#loc_t = np.random.multivariate_normal(my_plan[i], [[path_noise, 0], [0, path_noise]]) # name 't_i' i.e. t_1, t_2,...t_n
-			loc_x = Q.randn( mu=other_plan[i][0], sigma=path_noise, name="other_run_x_"+str(i) )
-			loc_y = Q.randn( mu=other_plan[i][1], sigma=path_noise, name="other_run_y_"+str(i) )
-			loc_t = [loc_x, loc_y]
-			other_noisy_plan.append(loc_t)
-		other_noisy_plan.append(other_plan[-1])
+		# # add noise to the plan
+		# other_noisy_plan = [other_plan[0]]
+		# for i in xrange(1, len(other_plan)-1):#loc_t = np.random.multivariate_normal(my_plan[i], [[path_noise, 0], [0, path_noise]]) # name 't_i' i.e. t_1, t_2,...t_n
+		# 	loc_x = Q.randn( mu=other_plan[i][0], sigma=path_noise, name="other_run_x_"+str(i) )
+		# 	loc_y = Q.randn( mu=other_plan[i][1], sigma=path_noise, name="other_run_y_"+str(i) )
+		# 	loc_t = [loc_x, loc_y]
+		# 	other_noisy_plan.append(loc_t)
+		# other_noisy_plan.append(other_plan[-1])
+		other_noisy_plan = [[0.42499999999999999, 0.40900000000000003], [0.42637252682699389, 0.45015681250166928], [0.42341590262456913, 0.4990041011765457], [0.4379044695823372, 0.53904127684124392], [0.45788032502031573, 0.55940630406732961], [0.50799614070063526, 0.55464417160785029], [0.54654058973382436, 0.5808784272257409], [0.55952256198212424, 0.61299850534228906], [0.583496600005313, 0.65140274446045765], [0.6005654644648678, 0.69540383781443371], [0.61589998114275823, 0.72963695272460394], [0.63340952989938293, 0.77334914393651799], [0.63415333146793929, 0.81788817596701702], [0.63380864206140508, 0.85068496120846326], [0.6574056904956691, 0.89840736011907418], [0.67365232860101742, 0.92626170086940862], [0.67272997642459853, 0.92476136486233518], [0.670977615078586, 0.9256403675603595], [0.67335036075493437, 0.92761813409888549], [0.67196251574528953, 0.92641384269803029], [0.67098919832850146, 0.92568243002410999], [0.67525871466320719, 0.9229149307167801], [0.67844855732075227, 0.92569329907319242], [0.67321881540381412, 0.93152732857257847], [0.67417822959121554, 0.9200035804784289], [0.67214122482315442, 0.92764945851654945], [0.67164236239673092, 0.92684256939487508], [0.67804513322197213, 0.92666665228204315], [0.6761799161969908, 0.92395809094667547], [0.67542733927721299, 0.92361716626504453], [0.67634231767336883, 0.92658004688912743], [0.67439978040601001, 0.92269389652124378], [0.67429561506189239, 0.92744438186414235], [0.67170106709439659, 0.92538278500943594], [0.67276872621711614, 0.91832099282710455], [0.67247922731692655, 0.92554210689024119], [0.67518132475675507, 0.92729301768019035], [0.6763367594646077, 0.92438316567133738], [0.67522650957951036, 0.92475789453983981], [ 0.675,  0.925]]
+	
 
 
 		#------------- model agent's own movements (past and future) --------------
